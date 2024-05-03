5da9a08cf190eee3bdeecb4e295e905cf60db6ab
NoahYannis <noah.guderjahn@gmx.de>
2024-05-03 10:49:40 +0200

    CSS #12 Navbar 1

--- index.php
+++ index.php
@@ -7,22 +7,28 @@
     <title>Buchladen PHP</title>
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <nav>
+            <form action="" method="post">
+                <button type="submit" name="displayTableButton" class="nav-button" value="autoren">autoren</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="buecher">buecher</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="lieferanten">lieferanten</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="orte">orte</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="sparten">sparten</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="verlage">verlage</button>
+
+                <button type="submit" name="displayTableButton" class="nav-button" value="autoren_has_buecher">autoren_has_buecher</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="buecher_has_sparten">buecher_has_sparten</button>
+                <button type="submit" name="displayTableButton" class="nav-button" value="buecher_has_lieferanten">buecher_has_lieferanten</button>
+            </form>
+        </nav>
+
+
 </head>
 
 <body>
 
-    <form action="" method="post">
-        <button type="submit" name="displayTableButton" class="button" value="autoren">autoren</button>
-        <button type="submit" name="displayTableButton" class="button" value="buecher">buecher</button>
-        <button type="submit" name="displayTableButton" class="button" value="lieferanten">lieferanten</button>
-        <button type="submit" name="displayTableButton" class="button" value="orte">orte</button>
-        <button type="submit" name="displayTableButton" class="button" value="sparten">sparten</button>
-        <button type="submit" name="displayTableButton" class="button" value="verlage">verlage</button>
-
-        <button type="submit" name="displayTableButton" class="button" value="autoren_has_buecher">autoren_has_buecher</button>
-        <button type="submit" name="displayTableButton" class="button" value="buecher_has_sparten">buecher_has_sparten</button>
-        <button type="submit" name="displayTableButton" class="button" value="buecher_has_lieferanten">buecher_has_lieferanten</button>
-    </form>
+    
 
     <form class='centered-container' style='margin-top: 40px; margin-bottom: 40px;' action='' method='post'>
         <input name="sql_input" type="text" placeholder="SQL-Statement eingeben..." />
